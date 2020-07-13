055ef5068ecb2e803700ec373e46ffa93fdd5835
MMcClure11 <mcclme02@luther.edu>
2020-07-13 17:00:57 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(name: "one")
-    @recipe.ingredients.build(name: "two")
+    @recipe.ingredients.build(name: "First Ingredient")
+    @recipe.ingredients.build(name: "Second Ingredient")
   end
 
   def create
